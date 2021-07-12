4326b8cf0f243bd17194c144d7ca38d31d4f522a
digui13 <www.eee.az@gmail.com>
2021-07-12 21:44:36 +0100

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_magical_stuff(char const *storage);
+
+#endif
